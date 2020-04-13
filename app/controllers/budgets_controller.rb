74e1cb04c2c8ff686e441b4e368dabcfcad70a7f
AhmadZain01 <ahmad.zain@xprolab.com>
2020-04-13 11:11:30 +0500

    income and expense method is added

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,40 @@
 class BudgetsController < ApplicationController
+  def index
+    @budgets = Budget.all
+  end
+
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+    @budget = Budget.new(budget_params)
+    @budget.save
+    redirect_to budgets_path(@budget)
+  end
+
+  def edit
+    @budget = Budget.find(params[:id])
+  end
+
+  def update
+    @budget = Budget.find(params[:id])
+    if @budget.update(budget_params)
+      flash[:notice] = "Budget was successfully updated"
+      redirect_to budgets_path(@budget)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @budget = Budget.find(params[:id])
+    @budget.destroy
+    flash[:danger] = "Budget was successfully deleted"
+   redirect_to budgets_path(@budget)
+  end
+
+  def budget_params
+    params.require(:budget).permit(:income, :expense, :description)
+  end
 end
